978096eff3f286a73ea0a614f99aa134f31e8e9d
Abraham Vazquez <mvazquezp1000@alumno.ipn.mx>
2020-11-15 02:26:11 -0600

    Initial Commit

---
+++ LoginNRegister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginNRegister
+//
+//  Created by Abraham Vazquez on 15/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
